1c7fbe1a89815aef05e8121cac36b2ae99258f03
Denys Severin <severindenys367@gmail.com>
2025-02-19 12:16:54 +0100

    added content for homepage component

--- src/pages/HomePage.tsx
+++ src/pages/HomePage.tsx
@@ -1,9 +1,31 @@
-import React from 'react'
+import { Heart, HeartPulse, Search, Soup } from "lucide-react";
+import React from "react";
+import RecipeCard from "../components/RecipeCard";
 
 const HomePage = () => {
   return (
-    <div>HomePage</div>
-  )
-}
+    <div className="bg-[#faf9fb] p-10 flex-1">
+      <div className="max-w-screen-lg mx-auto">
+        <form>
+          <label className="input shadow-md flex items-center gap-2 w-full max-w-lg">
+            <Search size={"24"} />
+            <input
+              type="text"
+              className="text-sm md:text-md grow flex w-full"
+              placeholder="What do you want to cook today?"
+            />
+          </label>
+        </form>
+        <p className="font-bold text-3xl md:text-5xl mt-4">Recommended Recipes</p>
+        <p className="text-slate-500 font-semibold ml-1 my-2 text-sm tracking-tight">Popular choices</p>
 
-export default HomePage
+        <div className="grid gap-3 grid-cols-1 md:grid-cols-2 lg:grid-cols-3">
+          {/* 1st recipe */}
+        <RecipeCard/>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default HomePage;
